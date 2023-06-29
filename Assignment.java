0d8489fa38bef7aa9800568374bf6e6a816b441a
Nguyen Luong Tien Dung <55966725+tiendung111@users.noreply.github.com>
2023-06-29 13:20:12 +0700

    Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -69,7 +69,7 @@ public class Assignment {
 		// Find all the item names on the page 1
 		List<WebElement> pageOneItemNames = driver.findElements(By.xpath("//div[@data-testid='spnSRPProdName']"));
 		// Print out the names of all the items on page 1
-		System.out.println("LIST OF ITEM NAMES FOR PAGE 1");
+		System.out.println("----------LIST OF ITEM NAMES FOR PAGE 1----------");
 		for (WebElement itemName : pageOneItemNames) {
 			System.out.println(itemName.getText());
 		}
@@ -83,9 +83,9 @@ public class Assignment {
 		// Find all the item names on the page 2
 		List<WebElement> pageTwoItemNames = driver.findElements(By.xpath("//div[@data-testid='spnSRPProdName']"));
 		// Print out the names of all the items on page 2
-		System.out.println("LIST OF ITEM NAMES FOR PAGE 2");
-		for (WebElement itemName : pageTwoItemNames) {
-			System.out.println(itemName.getText());
+		System.out.println("----------LIST OF ITEM NAMES FOR PAGE 2----------");
+		for (WebElement itemNameTwo : pageTwoItemNames) {
+			System.out.println(itemNameTwo.getText());
 		}
 
 		// Direct to page 3
@@ -97,9 +97,9 @@ public class Assignment {
 		// Find all the item names on the page 3
 		List<WebElement> pageThreeItemNames = driver.findElements(By.xpath("//div[@data-testid='spnSRPProdName']"));
 		// Print out the names of all the items on page 3
-		System.out.println("LIST OF ITEM NAMES FOR PAGE 3");
-		for (WebElement itemName : pageThreeItemNames) {
-			System.out.println(itemName.getText());
+		System.out.println("----------LIST OF ITEM NAMES FOR PAGE 3----------");
+		for (WebElement itemNameThree : pageThreeItemNames) {
+			System.out.println(itemNameThree.getText());
 		}
 	}
 
